5da0e41ee1e24dc3b5b38bad675484ea0d346af0
Finble <elainefinn@hotmail.com>
2016-03-01 10:43:45 +0000

    Routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'restaurants' => 'restaurants#index'  # links up the '/restaurants' URL to the index in the restaurants controller
+  resources :restaurants
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

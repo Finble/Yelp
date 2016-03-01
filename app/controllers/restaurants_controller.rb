fa6774fcda0f0f36ddf09b5686f2e7aa6247122a
Finble <elainefinn@hotmail.com>
2016-03-01 17:44:31 +0000

    Add description and show name of restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,7 +13,12 @@ class RestaurantsController < ApplicationController
 		redirect_to '/restaurants'
 	end
 
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
 	def restaurant_params
 		params.require(:restaurant).permit(:name)
 	end
+
 end

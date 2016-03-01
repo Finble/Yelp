82297a1cdd5b9445d49049332aa07e0313948b69
Wim Hekkelman <j.hekkelman4@upcmail.nl>
2016-03-01 12:48:01 +0100

    added restaurant model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 feature 'restaurants' do
 
 	context 'no restaurants have been added' do
+		before do
+			Restaurant.create(name: 'KFC')
+		end
+
+		scenario 'display restaurants' do
+			visit '/restaurants'
+			expect(page).to have_content('KFC')
+			expect(page).not_to have_content('No restaurants yet')
+		end
+
 		scenario 'should display a prompt to add a restaurant' do
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants yet'
